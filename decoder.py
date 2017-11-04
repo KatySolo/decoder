3a0384ede4980a309bbcb5dc0187cac491828a79
Solodovnikova Ekaterina <colo18@yandex.ru>
2017-11-04 23:41:53 +0500

    added method for separation into buckets

--- decoder.py
+++ decoder.py
@@ -4,6 +4,20 @@ import re
 def split_text(input_text):
     return re.findall(r"[\w']+", input_text)
 
+
+def separate_to_buckets(input_words_array):
+    buckets = {}
+    for word in input_words_array:
+        for key in buckets.keys():
+            if key != len(word):
+                buckets[len(word)] = {word}
+                break
+            else:
+                buckets[len(word)].append(word)
+                break
+    return buckets
+
+
 def transform_to_number_sequence (input_string):
     letters = {}
     number_sequence = ""
--- tests.py
+++ tests.py
@@ -10,13 +10,23 @@ class TestTransformToNumberSequence (unittest.TestCase):
         number_sequence = "01203040120"
         self.assertEqual(decoder.transform_to_number_sequence(input_string),number_sequence)
 
-class TestSplitTest (unittest.TestCase):
+
+class TestSplitText (unittest.TestCase):
 
     def test_split_test(self):
         input_text = "Hi! My name is Kate? I am nineteen years old, are not I?"
         splitted_text = ["Hi","My","name","is","Kate","I","am","nineteen","years","old","are","not","I"]
         actual = decoder.split_text(input_text)
         self.assertEqual(actual,splitted_text)
+
+
+class TestSeparationToBuckets(unittest.TestCase):
+
+    def separate_to_buckets_test(self):
+        input_words_array = ["Hi","My","name","is","Kate","I","am","nineteen","years","old","are","not","I"]
+        expected = {'1':{'I','I'},'2':{'Hi','My','is','am'},'3':{'old','are','not'},'4':{'name','Kate'},'5':{'years'},'8':{'nineteen'}}
+        self.assertEqual(decoder.separate_to_buckets(input_words_array),expected)
+
 
 if __name__ == '__main__':
     unittest.main()
