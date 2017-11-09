deeb9caa3b483356066fb36f4c1a71646950946c
Solodovnikova Ekaterina <colo18@yandex.ru>
2017-11-09 14:20:49 +0500

    make test more similar

--- tests.py
+++ tests.py
@@ -7,25 +7,27 @@ class TestTransformToNumberSequence (unittest.TestCase):
 
     def test_transform_to_number_sequence(self):
         input_string = 'abracadabra'
-        number_sequence = "01203040120"
-        self.assertEqual(decoder.transform_to_number_sequence(input_string),number_sequence)
+        actual = decoder.transform_to_number_sequence(input_string)
+        expected = "01203040120"
+        self.assertEqual(actual,expected)
 
 
 class TestSplitText(unittest.TestCase):
 
     def test_split_test(self):
         input_text = "Hi! My name is Kate? I am nineteen years old, are not I?"
-        splitted_text = ["Hi","My","name","is","Kate","I","am","nineteen","years","old","are","not","I"]
         actual = decoder.split_text(input_text)
-        self.assertEqual(actual,splitted_text)
+        expected = ["Hi","My","name","is","Kate","I","am","nineteen","years","old","are","not","I"]
+        self.assertEqual(actual,expected)
 
 
 class TestSeparationToBuckets(unittest.TestCase):
 
     def test_separate_to_buckets(self):
         input_words_array = ["Hi","My","name","is","Kate","I","am","nineteen","years","old","are","not","I"]
+        actual =  decoder.separate_to_buckets(input_words_array)
         expected = {1:['I','I'],2:['Hi','My','is','am'],3:['old','are','not'],4:['name','Kate'],5:['years'],8:['nineteen']}
-        self.assertEqual(decoder.separate_to_buckets(input_words_array),expected)
+        self.assertEqual(actual,expected)
 
 
 class TestTransformationBucketsToSequence(unittest.TestCase):
@@ -33,7 +35,7 @@ class TestTransformationBucketsToSequence(unittest.TestCase):
     def test_transform_buckets_to_sequence_less_than_hundred(self):
         expected = {1:['0','0'],2:['01','01','01','01'], 3:['012','012','012'],4:['0123','0123'],5:['01234'],8:['01023220']}
         actual = decoder.extract_hundred_sequence_from_buckets({1:['I','I'],2:['Hi','My','is','am'],3:['old','are','not'],4:['name','Kate'],5:['years'],8:['nineteen']})
-        self.assertEqual(expected,actual)
+        self.assertEqual(actual,expected)
 
     def test_transform_buckets_to_sequence_more_than_hundred(self):
         buckets = {}
@@ -42,7 +44,9 @@ class TestTransformationBucketsToSequence(unittest.TestCase):
         three_letters_sequences = ['010' for i in range (100)]
         buckets[3] = three_letters_words
         sequence_buckets[3] = three_letters_sequences
-        self.assertEqual(sequence_buckets, decoder.extract_hundred_sequence_from_buckets(buckets))
+        actual = decoder.extract_hundred_sequence_from_buckets(buckets)
+        expected = sequence_buckets
+        self.assertEqual(actual, expected)
 
 class TestImportBasicEnglishWords(unittest.TestCase):
 
